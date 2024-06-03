419e2b14e5b633856ca2394b252a02556606915f
Tom Richards <30478573+GrabAByte@users.noreply.github.com>
2024-06-03 10:08:23 +0100

    fix: Add configuration for parent Jenkinsfile, and include scriptPath to Jenkinsfile for all seed jobs (#5)

--- seed/jobs.groovy
+++ seed/jobs.groovy
@@ -4,9 +4,32 @@ def List envs     = ["dev", "staging", "prod"]
 def List projects = ["A", "B", "C"]
 def String id     = 100000000 + random.nextInt(900000000)
 
+// Add main upstream job that performs remote trigger to downstream jobs
+multibranchPipelineJob("POC/Upstream-Check-For-Changes-Pipeline") {
+    branchSources {
+        git {
+            id("${id}")
+            remote('https://github.com/GrabAByte/jenkins-mono-concept.git')
+            credentialsId('github-ci')
+            includes('*')
+        }
+    }
+    factory {
+        workflowBranchProjectFactory {
+            scriptPath('Jenkinsfile')
+        }
+    }
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(5)
+        }
+    }
+}
+
+// Add all downstream jobs
 for (env in envs) {
     for (project in projects) {
-        multibranchPipelineJob("POC/${env}/${project}/Check-For-Changes-Pipeline")
+        multibranchPipelineJob("POC/${env}/${project}/Check-For-Changes-Pipeline") {
             branchSources {
                 git {
                     id("${id}")
@@ -15,9 +38,15 @@ for (env in envs) {
                     includes('*')
                 }
             }
-        orphanedItemStrategy {
-            discardOldItems {
-                numToKeep(5)
+            factory {
+                workflowBranchProjectFactory {
+                    scriptPath("environments/${env}/${project}/Jenkinsfile")
+                }
+            }
+            orphanedItemStrategy {
+                discardOldItems {
+                    numToKeep(5)
+                }
             }
         }
     }
