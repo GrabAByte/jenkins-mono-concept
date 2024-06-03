48912a2b7e20e9e2f44a0d5ba91f7cd3f0772b6d
Tom Richards <twr.devops@gmail.com>
2024-06-03 10:00:34 +0100

    fix: Add parent jenkins job

--- seed/jobs.groovy
+++ seed/jobs.groovy
@@ -4,9 +4,27 @@ def List envs     = ["dev", "staging", "prod"]
 def List projects = ["A", "B", "C"]
 def String id     = 100000000 + random.nextInt(900000000)
 
+// Add main upstream job that performs remote trigger to downstream jobs
+multibranchPipelineJob("POC/Upstream-Check-For-Changes-Pipeline") {
+    branchSources {
+        git {
+            id("${id}"i)
+            remote('https://github.com/GrabAByte/jenkins-mono-concept.git')
+            credentialsId('github-ci')
+            includes('*')
+        }
+    }
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(5)
+        }
+    }
+}
+
+// Add all downstream jobs
 for (env in envs) {
     for (project in projects) {
-        multibranchPipelineJob("POC/${env}/${project}/Check-For-Changes-Pipeline")
+        multibranchPipelineJob("POC/${env}/${project}/Check-For-Changes-Pipeline") {
             branchSources {
                 git {
                     id("${id}")
@@ -15,9 +33,10 @@ for (env in envs) {
                     includes('*')
                 }
             }
-        orphanedItemStrategy {
-            discardOldItems {
-                numToKeep(5)
+            orphanedItemStrategy {
+                discardOldItems {
+                    numToKeep(5)
+                }
             }
         }
     }
