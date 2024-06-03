61b4f6d74d1c4605b56cf3aa982f528fc09b6172
Tom Richards <twr.devops@gmail.com>
2024-06-03 09:33:42 +0100

    feat: Add seed jobs to mono concept repository

--- seed/jobs.groovy
+++ seed/jobs.groovy
@@ -1,1 +1,24 @@
-#
+#!/usr/bin/env groovy
+// PSEUDO CODE ONLY
+def List envs     = ["dev", "staging", "prod"]
+def List projects = ["A", "B", "C"]
+def String id     = 100000000 + random.nextInt(900000000)
+
+for (env in envs) {
+    for (project in projects) {
+        multibranchPipelineJob("POC/${env}/${project}/Check-For-Changes-Pipeline")
+            branchSources {
+                git {
+                    id("${id}")
+                    remote('https://github.com/GrabAByte/jenkins-mono-concept.git')
+                    credentialsId('github-ci')
+                    includes('*')
+                }
+            }
+        orphanedItemStrategy {
+            discardOldItems {
+                numToKeep(5)
+            }
+        }
+    }
+}
